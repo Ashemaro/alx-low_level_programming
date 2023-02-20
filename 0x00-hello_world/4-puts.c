1dc62412770210bd7f54fc6d6b198538bff0050c
Kudakwashe <benjykuda@gmail.com>
2023-02-20 10:22:07 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-
+/**
+ *  * main - A program that prints 'Programming is like building a multilingual puzzle' with puts function
+ *    */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
