f103a31ca001d1b95d235269f7744e9de80bdb57
Kudakwashe <benjykuda@gmail.com>
2023-02-21 15:53:21 -0800

    added 4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _isalpha - checks if a character is a letter
+ * @c - integer value of the character
+ * Return: returns 1 if character is a letter else 0
+ */
+
+int _isalpha(int c)
+{
+	if (c < 97)
+	{
+		if ( c < 91)
+		{
+			if (c >= 65)
+			{
+				return (1);
+			}
+			else
+			{
+				return (0);
+			}
+		}
+	}
+	else if ( c > 122)
+	{
+		return (0);
+	}
+}
