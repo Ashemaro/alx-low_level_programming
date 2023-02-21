3216cbb43aca3ed53f8277c043bac6023577bbf0
Kudakwashe <benjykuda@gmail.com>
2023-02-21 14:38:54 -0800

    Added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *main - prints the alphabet in lowercase letters
+ *Return : No return value
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+		_putchar(i);
+
+	_putchar(10)
+}
