caa2fd2eaa355bc626134d244707fd42b151bdb9
Kudakwashe <benjykuda@gmail.com>
2023-02-21 14:09:12 -0800

    Printing '_putchar' using -putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,6 +5,11 @@
 **/
 
 int main(void){
-	_putchar("_putchar");
+	int i;
+	int str_array[8] = {95, 112, 117, 116, 99,104, 97, 114};
+	for(i=0; i<=7; i++){
+		_putchar(str_array[i]);
+	}
+	_putchar(10);
 	return (0);
 }
