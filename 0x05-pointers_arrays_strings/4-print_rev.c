c929704c49fbcdfbc065ef1bb0cdad4b6965c370
Kudakwashe <benjykuda@gmail.com>
2023-02-27 13:46:55 -0800

    Ammended episode 4:

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,3 +1,25 @@
 #include "main.h"
 /**
- * print_rev - 
+ * print_rev - reverses a string then a new line
+ * @s: string to be reversed
+ * return: returns nothing
+ */
+
+void print_rev(char *s)
+{
+	int longi = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+	for (o = longi; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
