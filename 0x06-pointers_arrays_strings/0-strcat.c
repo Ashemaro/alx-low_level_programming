5633154a48a6270e21b13d603400baf747363410
Kudakwashe <benjykuda@gmail.com>
2023-03-02 13:07:12 -0800

    Added 0-strcat.c file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcat - appends the src string to the dest string,
+ * overwriting the terminating null byte (\0) at the end
+ * of dest, and then adds a terminating null byte.
+ * @*dest: input
+ * @*src: input
+ *Return: Returns a pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src);
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strncat - function appends the src string to the dest string,
+ * overwriting the terminating null byte (\0) at the end of dest.
+ * Uses at most n bytes from src and src does not need to be
+ * null-terminated.
+ * @*dest: 
+ * @*src:
+ * Return: Return a pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n);
+{
+	
+}
