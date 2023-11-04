8858007fd738f86fa62b91a875567cbf5c68b3c7
Ashe <benjykuda@gmail.com>
2023-11-04 16:39:04 -0700

    Created 1-args.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Prints out the name of the program
+ * return - returns no value
+ */
+
+int main(int argc, char *argv[])
+{
+        printf("%s \n", argv[0]);
+        return 0;
+}
+
